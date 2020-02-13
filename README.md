bbb5bed50dff139ed2c674dbed74631177a8d7ce
tomiriszhashibekova <tomiris_zh.01@mail.ru>
2020-02-13 22:12:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-hpzkjq-rrqety
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-hpzkjq-rrqety)
---
+++ src/app/products.ts
@@ -0,0 +1,82 @@
+export const products = [
+  {
+     name: 'BMAX S15',
+    price: 299.00,
+    rating: '4.9/5',
+    description: 'Laptop 15.6 inch Intel Gemini Lake N4100 Intel UHD Graphics 600 8GB LPDDR4 RAM 128GB SSD 178° Viewing Angle Notebook',
+    image: 'https://ae01.alicdn.com/kf/H08e0c9593bff4a5a9da7472ec93e9d14h/BMAX-S15-15-6-Intel-Gemini-Lake-N4100-Intel-UHD-graphics-600-8.jpg_50x50.jpg',
+    ref: 'https://aliexpress.ru/item/4000674877924.html?spm=a2g0o.cart.0.0.50563c00fkdV15&mp=1'
+  },
+  {
+    name: 'T-BAO TBOOK X8S',
+    price: 339.99,
+    rating: '5.0/5',
+    description: 'Laptop 15.6 inch Intel Celeron J3455 8GB LPDDR4 256GB SSD Intel HD Graphics 500',
+    image: 'https://ae01.alicdn.com/kf/HLB1U69JTwHqK1RjSZFgq6y7JXXax/T-BAO-TBOOK-X8S-15-6-Intel-Celeron-J3455-8-LPDDR4-256-SSD.jpg_50x50.jpg',
+    ref:'https://aliexpress.ru/item/33008209145.html?spm=a2g0o.cart.0.0.50563c00fkdV15&mp=1'
+  },
+  {
+    name: 'PanTong 2019 model Apple MacBook Air 13',
+    price: 1500,
+    rating: '5.0/5',
+     description: 'inch 128G silver/space gray/gold Apple Authorized Online Seller',
+    image: 'https://ae01.alicdn.com/kf/HTB1brgSaCf2gK0jSZFPq6xsopXa9/PanTong-2019-Apple-MacBook-Air-13-128G.jpg_50x50.jpg',
+    ref: 'https://www.aliexpress.ru/item/4000014299035.html?spm=a2g0o.cart.0.0.50563c00fkdV15&mp=1'
+  },
+  {
+     name: 'PanTong 2019 model Apple MacBook Pro 16',
+    price: 3551,
+    rating: '4.6/5',
+      description: ' inch 1T integrated Touch ID sensor Apple Authorized Online Seller',
+    image: 'https://ae01.alicdn.com/kf/HTB1_.MPapY7gK0jSZKzq6yikpXaO/PanTong-2019-Apple-MacBook-Air-13-128G.jpg_50x50.jpg',
+    ref: 'https://www.aliexpress.ru/item/4000014299035.html?spm=a2g0o.cart.0.0.50563c00fkdV15&mp=1'
+  },
+  {
+     name: 'Teclast F5 11.6',
+    price: 309.99,
+    rating:'5.0/5',
+    description: 'Touch Screen Laptop 8GB DDR4 256GB SSD Windows 10 Notebook Intel Gemini Lake FHD Display 360° Rotation Computer',
+    image:'https://ae01.alicdn.com/kf/Hfb2cdaf1372a4195be563a4c354e5cc2A/Teclast-F5-11-6-8-DDR4-256-SSD-Windows.jpg_50x50.jpg',
+    ref: 'https://aliexpress.ru/item/4000594508116.html?spm=a2g0o.productlist.0.0.595b7df4oV4U9V&s=p&algo_pvid=0cabef9e-a533-4f9a-8d97-e3a956e5d64d&algo_expid=0cabef9e-a533-4f9a-8d97-e3a956e5d64d-3&btsid=a1386737-3e9d-41fa-ae1c-b3f81a09ae3a&ws_ab_test=searchweb0_0,searchweb201602_8,searchweb201603_55'
+  },
+  {
+    name: 'CHUWI LapBook Pro 14.1',
+    price: 344.07,
+    rating:'4.9/5',
+    description: 'Inch Intel Gemini-Lake N4100 Quad Core 8GB RAM 256GB SSD Windows 10 Laptop with Backlit Keyboard',
+    image: 'https://ae01.alicdn.com/kf/Hab7c3d3ac0a04747839727a5c15681297/Teclast-F5-11-6-8-DDR4-256-SSD-Windows.jpg_50x50.jpg',
+    ref: 'https://aliexpress.ru/item/4000098061056.html?spm=a2g0o.cart.0.0.50563c00fkdV15&mp=1'
+  },
+  {
+    name: 'laptop 15.6',
+    price: 336.36,
+    rating:'5.0/5',
+     description: 'inch 8GB RAM 128GB 512GB 1TB SSD CPU Inte i7 Gaming Laptop Ultrabook intel Quad Core Win10 Notebook Computer',
+    image: 'https://ae01.alicdn.com/kf/HTB1mxbvXoH1gK0jSZSyq6xtlpXa4/CHUWI-LapBook-Pro-14-1-Intel-Gemini-Lake-N4100.jpg_50x50.jpg',
+    ref:'https://aliexpress.ru/item/32982839253.html?spm=a2g0o.cart.0.0.50563c00fkdV15&mp=1'
+  },
+  {
+    name: '15.6 inch Laptop CPU i7 Ultrabook',
+    price: 441.93,
+    rating: '5.0/5',
+    description: 'intel Quad Core Win10 8GB RAM 1TB 512GB 256GB 128GB SSD Notebook Computer Gaming Laptop',
+    image:'https://ae01.alicdn.com/kf/HTB1Q5e5J4TpK1RjSZFMq6zG_VXaS/CHUWI-LapBook-Pro-14-1-Intel-Gemini-Lake-N4100.jpg_50x50.jpg',
+    ref:'https://aliexpress.ru/item/33058463426.html?spm=a2g0o.productlist.0.0.74585779ZysCYl&algo_pvid=61c60756-2484-4390-86d3-1e64e90b0161&algo_expid=61c60756-2484-4390-86d3-1e64e90b0161-14&btsid=57a77e84-d22a-43c6-98c4-768e705445ed&ws_ab_test=searchweb0_0,searchweb201602_8,searchweb201603_55'
+  },
+  {
+    name: 'laptop 8GB RAM 512GB',
+    price: 442.17,
+    rating: '4.0/5',
+    description: ' 256GB 512GB 1TB SSD Windows 10 15.6 Inch Notebook Intel i7 CPU 1080P Full Size Layout Keyboard gaming',
+    image:'https://ae01.alicdn.com/kf/Hbb9b4325e3bc447ba0a4e1a311a628f7p/8-512-256-512-1-SSD-Windows-10-15.jpg_50x50.jpg',
+    ref:'https://aliexpress.ru/item/4000255615452.html?spm=a2g0o.cart.0.0.50563c00fkdV15&mp=1'
+  },
+  {
+   name: 'Gaming Laptop 8GB 240GB Ram 15.6',
+    price: 212.19,
+    rating:'4.7/5',
+     description: 'inch 1920*1080P large Screen Intel CPU Windows 10 System Fast Boot Notebook Computer',
+    image: 'https://ae01.alicdn.com/kf/H5d08380372914b5ba9150aeb14836e222/15-6-i7-intel-4-Win10-8.jpg_50x50.jpg',
+    ref: 'https://aliexpress.ru/item/4000255718489.html?spm=a2g0o.cart.0.0.50563c00fkdV15&mp=1'
+  }
+]; 
